35e13074f5f73437eb22fdaf7f025ed33ccf19ab
Edgar Rivera <edgar.a.rivera08@gmail.com>
2020-02-03 13:35:54 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,7 +3,12 @@
 
 def meal_choice(veg1, veg2, protein="meat")
   puts "What a nutritious meal!"
+<<<<<<< HEAD
   order = "A plate of #{protein} with #{veg1} and #{veg2}." #created to output a value outside the method
   puts order
   order #implicit return used; explicited would be return order
+=======
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+  return "A plate of #{protein} with #{veg1} and #{veg2}."
+>>>>>>> e014b0d302d5091f4f0e25563334899291e73197
 end
